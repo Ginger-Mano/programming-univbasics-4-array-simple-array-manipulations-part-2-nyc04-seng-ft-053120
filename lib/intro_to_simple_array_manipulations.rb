c7859ef42424f1c42b0399df1186a35d9159bb0b
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-06 02:33:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,5 +3,17 @@ def using_concat(a, b)
 end
 
 def using_insert(array, element)
-  array.insert[4] << element 
+  array.insert[4] << element
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
 end
